3ab0b6cb4f4e4ca524828eeb15ad18c26f4f1745
Sabbir Ahmmed <engineersabbir.dev@gmail.com>
2023-08-16 08:29:59 +0600

    deposit.js file updated

--- js/deposit.js
+++ js/deposit.js
@@ -1,38 +1,16 @@
-function getInputValueById(inputId) {
-    const inputField = document.getElementById(inputId);
-    const inputFieldString = inputField.value;
-    const inputValue = parseFloat(inputFieldString);
-    inputField.value = '';
-    return inputValue;
-}
-
-function getElementPreviousValueById(elementId) {
-    const element = document.getElementById(elementId);
-    const elementValueString = element.innerText;
-    const elementValue = parseFloat(elementValueString);
-    return elementValue;
-}
-
-function setElementValueById(elementId, addedValue){
-    const newValue = document.getElementById(elementId);
-    newValue.innerText = addedValue;
-}
-
-
-
 document.getElementById('btn-deposit').addEventListener('click', function () {
-    // deposit balance calculation:
     const depositInputValue = getInputValueById('deposit-input');
-    
     const previousDepositAmount = getElementPreviousValueById('deposit-total');
+    const newBalanceTotal= getElementPreviousValueById('balance-total');
+
+    if(isNaN(depositInputValue)){
+        alert('Please provide a valid number');
+        return;
+    }
 
     const newDepositTotal = previousDepositAmount + depositInputValue;
-
     setElementValueById('deposit-total', newDepositTotal);
 
-    //total balance calculation:
-    const newBalanceTotal= getElementPreviousValueById('balance-total');
     const newBalance = newBalanceTotal + depositInputValue;
-
     setElementValueById('balance-total', newBalance);
 })
